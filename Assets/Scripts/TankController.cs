494d19760f6844b439f2fdffc300f6683fc3b41f
Nazarov Andrey <nazandrbox@yandex.ru>
2016-11-26 21:58:55 +0300

    Changing keys for tank movings

--- Assets/Scripts/TankController.cs
+++ Assets/Scripts/TankController.cs
@@ -14,19 +14,19 @@ public class TankController : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKey (KeyCode.W)) {
+		if (Input.GetKey (KeyCode.UpArrow)) {
 			gameObject.transform.Translate (new Vector3 (0, 0, speed));
 		} 
 
-		if (Input.GetKey (KeyCode.S)) {
+		if (Input.GetKey (KeyCode.DownArrow)) {
 			gameObject.transform.Translate (new Vector3 (0, 0, -speed));
 		}
 
-		if (Input.GetKey (KeyCode.A)) {
+		if (Input.GetKey (KeyCode.LeftArrow)) {
 			gameObject.transform.Rotate(new Vector3 (0, -maneuverability, 0));
 		} 
 
-		if (Input.GetKey (KeyCode.D)) {
+		if (Input.GetKey (KeyCode.RightArrow)) {
 			gameObject.transform.Rotate(new Vector3 (0, maneuverability, 0));
 		}
 

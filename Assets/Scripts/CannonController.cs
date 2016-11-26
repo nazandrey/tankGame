fbc2093696d539d6a41bfcea812ae31a2b0ac39b
Nazarov Andrey <nazandrbox@yandex.ru>
2016-11-26 21:09:01 +0300

    Applying Unity git settings

--- Assets/Scripts/CannonController.cs
+++ Assets/Scripts/CannonController.cs
@@ -1,35 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class CannonController : MonoBehaviour {
-	public GameObject bulletTemplate;
-
-	private const float DelayBetweenShots = 1.0f;
-	private const int BulletStartDistance = 3;
-	private float currentDelayBetweenShots = 0.0f;
-	private bool canShoot = true;
-
-	public void Shoot(){
-		//debounce
-		if (canShoot) {
-			Vector3 bulletPosition = gameObject.transform.position + gameObject.transform.forward * BulletStartDistance;
-			Instantiate (bulletTemplate, bulletPosition, gameObject.transform.rotation);
-			currentDelayBetweenShots = DelayBetweenShots;
-		}
-	}
-
-	// Use this for initialization
-	void Start () {
-
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		if (currentDelayBetweenShots > 0) {
-			canShoot = false;
-			currentDelayBetweenShots -= Time.deltaTime;
-		} else {
-			canShoot = true;
-		}
+public class CannonController : WeaponController {
+	public CannonController() : base(2,3) {
 	}
 }

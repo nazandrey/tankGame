837658dc6bd6306a053c83f07c7ac215f9892be4
nazandrey <nazandrbox@yandex.ru>
2017-07-04 22:12:54 +0300

    #3 Rename MachineGunController to MachineGunShooter

--- Assets/Scripts/MachineGunController.cs
+++
@@ -1,7 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class MachineGunController : WeaponController {
-	public MachineGunController() : base(0.5f,2) {
-	}
-}
---
+++ Assets/Scripts/MachineGunShooter.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MachineGunShooter : GunShooter {
+	public MachineGunShooter() : base(0.5f,2f) {
+	}
+}

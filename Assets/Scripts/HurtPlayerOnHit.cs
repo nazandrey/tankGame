96605b4c115ef7640fe2c6b449d1d1abd8cf5797
nazandrey <nazandrbox@yandex.ru>
2017-07-04 22:16:44 +0300

    #7 Clear unused code

--- Assets/Scripts/HurtPlayerOnHit.cs
+++ Assets/Scripts/HurtPlayerOnHit.cs
@@ -4,12 +4,6 @@ using System.Collections;
 public class HurtPlayerOnHit : MonoBehaviour {
 	public float attackDamage = 4;
 
-	private Health _health;
-
-	void Start () {
-		_health = gameObject.GetComponent<Health> ();
-	}
-
 	void OnTriggerEnter(Collider other) {
 		if (other.tag == "Player") {
 			Health playerHealth = other.GetComponent<Health> ();

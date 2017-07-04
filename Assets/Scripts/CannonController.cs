804a51e69cc3aa7198863c4caf63bb6802005ef9
nazandrey <nazandrbox@yandex.ru>
2017-07-04 22:12:32 +0300

    #3 Rename CannonController to CannonShooter

--- Assets/Scripts/CannonController.cs
+++
@@ -1,7 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class CannonController : WeaponController {
-	public CannonController() : base(1,3){		
-	}
-}
---
+++ Assets/Scripts/CannonShooter.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CannonShooter : GunShooter {
+	public CannonShooter() : base(1f,3f){
+	}
+}
